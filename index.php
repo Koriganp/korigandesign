fe9890fa37944112190c4df8ca3ae9d2264a64a6
Korigan Payne <koriganp@gmail.com>
2018-01-22 16:05:55 -0700

    Made head data

--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+		<!-- Metadata -->
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="author" content="Korigan Payne"/>
+		<meta name="description" content="Personal website of the Albuquerque web developer Korigan Payne, featuring skills in web design and photography, as well as contact page."/>
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!-- Google Fonts -->
+
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+
+		<!-- Font Awesome -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+		<!-- Animate.css -->
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+
+		<!-- My CSS -->
+
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Page Title -->
+
+		<title>Korigan Design | Media and Web Design</title>
 
 	</head>
 	<body>
