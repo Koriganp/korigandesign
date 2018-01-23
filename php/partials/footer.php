b9b4a3c4f576151d2792ccc83738519861eda681
Korigan Payne <koriganp@gmail.com>
2018-01-23 12:56:05 -0700

    broke pages into partials for php injection

---
+++ php/partials/footer.php
@@ -0,0 +1,9 @@
+		</div>
+
+		<footer class="footer mt-0">
+			<div class="bg-light text-dark nav justify-content-center p-5">
+				<p>&copy; 2018<a href="https://koriganpayne.com"> Korigan Payne</a></p>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ php/partials/header.php
@@ -0,0 +1,25 @@
+
+<header>
+	<nav class="navbar navbar-default navbar-expand-md navbar-light bg-light">
+		<a class="navbar-brand" href="#"><img class="nav-icon" src="<?php echo $PREFIX;?>images/KD.png"> Korigan Design</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarToggler">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a href="<?php echo $PREFIX;?>index.php">Home</a>
+				</li>
+				<li class="nav-item">
+					<a href="<?php echo $PREFIX;?>services/index.php">Services</a>
+				</li>
+				<li class="nav-item">
+					<a href="<?php echo $PREFIX;?>about/index.php">About</a>
+				</li>
+				<li class="nav-item">
+					<a href="<?php echo $PREFIX;?>contact/index.php">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
