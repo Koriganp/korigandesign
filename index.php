c75cea889ddc1d295e19d0a84ba2cad8c6d9629c
Korigan Payne <koriganp@gmail.com>
2018-01-24 12:30:57 -0700

    working on home page

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ require_once("php/partials/head-utils.php");
 	<!-- Navigation Bar -->
 
 <?php require_once("php/partials/header.php");?>
+<div class="container">
+	<div class="row">
+		<div class="col-5">
+			<h1>Custom Media Solutions</h1>
+		</div>
+		<div class="col-7">
+		</div>
+	</div>
+</div>
 
 	<!-- Footer -->
 
