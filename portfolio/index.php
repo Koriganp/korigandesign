419509943769d8a616b2d2c7605e738e51a321db
Korigan Payne <Koriganp@gmail.com>
2018-02-14 10:29:05 -0700

    remade portfolio page

--- portfolio/index.php
+++ portfolio/index.php
@@ -22,49 +22,57 @@ require_once("../php/partials/head-utils.php");
 
 	<!-- Main Content -->
 
-	<main class="featured-work">
+	<main>
 
 		<!-- C Square Custom Cupcakes -->
 
 		<div class="container bg-white">
-			<div class="row">
-				<div class="col-md-5 cupcakes cupcakes-effects">
-					<div class="overlay-bottom">
-						<div class="text-bottom">
-							<p><strong>Problem:</strong> Not showing up on Google or Bing Searches</p>
-							<p><strong>Solution:</strong> Custom SEO to optimize search results to include this website in searches for custom cupcakes.</p>
-							<p><strong>Our Work:</strong> Search Engine Optimization</p>
-							<p><strong>Go There: </strong><a href="http://csquarecustomcupcakes.com/"  target="_blank">CSquareCustomCupcakes.com</a></p>
-						</div>
-					</div>
+			<div class="row py-5 my-5">
+				<div class="col-md-6">
+					<img src="<?php echo $PREFIX;?>images/csquare.jpg" alt="C Square Custom Cupcakes" class="portfolio-image"/>
 				</div>
-
-				<!-- Korigan Payne -->
-
-				<div class="col-md-7 korigan korigan-effects">
-					<div class="overlay-left">
-						<div class="text-left">
-							<p><strong>Problem:</strong> Needed a custom multi-page website</p>
-							<p><strong>Solution:</strong> Created a site complete with contact form and external authentication from scratch.</p>
-							<p><strong>Our Work:</strong> Full Site Development</p>
-							<p><strong>Go There: </strong><a href="https://koriganpayne.com/" target="_blank">KoriganPayne.com</a></p>
-						</div>
-					</div>
+				<div class="col-md-6 text-dark px-3">
+					<h3 class="portfolio-title my-4"><span class="first-letter">C<sup>2</sup></span> Custom Cupcakes</h3>
+					<p><strong>Problem:</strong> Not showing up on Google or Bing Searches</p>
+					<p><strong>Solution:</strong> Custom SEO to optimize search results to include this website in searches for custom cupcakes.</p>
+					<p><strong>Our Work:</strong> Search Engine Optimization</p>
+					<p><strong>Go There: </strong><a href="http://csquarecustomcupcakes.com/"  target="_blank">CSquareCustomCupcakes.com</a></p>
 				</div>
 			</div>
+		</div>
+		<hr>
 
-			<!-- Design By Korigan -->
+		<!-- Korigan Payne -->
 
-			<div class="row">
-				<div class="col-md-12 design-by-korigan design-by-korigan-effects">
-					<div class="overlay-left-design-by-korigan">
-						<div class="text-left-design-by-korigan">
-							<p><strong>Problem:</strong> Needed Wordpress Site</p>
-							<p><strong>Solution:</strong> Used a Themify template to provide a custom design and ease of maintaining.</p>
-							<p><strong>Our Work:</strong> Word Press installation and upkeep. Web hosting.</p>
-							<p><strong>Go There: </strong><a href="https://designbykorigan.com/" target="_blank">DesignByKorigan.com</a></p>
-						</div>
-					</div>
+		<div class="container bg-white">
+			<div class="row py-5 my-5">
+				<div class="col-md-6 order-lg-12">
+					<img src="<?php echo $PREFIX;?>images/korigan.jpg" alt="Korigan Payne Personal Website" class="portfolio-image"/>
+				</div>
+				<div class="col-md-6 order-lg-1 text-dark px-3">
+					<h3 class="portfolio-title my-4"><span class="first-letter">P</span>ersonal Website Design</h3>
+					<p><strong>Problem:</strong> Needed a custom multi-page website</p>
+					<p><strong>Solution:</strong> Created a site complete with contact form and external authentication from scratch.</p>
+					<p><strong>Our Work:</strong> Full Site Development</p>
+					<p><strong>Go There: </strong><a href="https://koriganpayne.com/" target="_blank">KoriganPayne.com</a></p>
+				</div>
+			</div>
+		</div>
+		<hr>
+
+		<!-- Design By Korigan -->
+
+		<div class="container bg-white">
+			<div class="row py-5 my-5">
+				<div class="col-md-6">
+					<img src="<?php echo $PREFIX;?>images/wp.jpg" alt="Design By Korigan" class="portfolio-image"/>
+				</div>
+				<div class="col-md-6 text-dark px-3">
+					<h3 class="portfolio-title my-4"><span class="first-letter">W</span>ordpress Website</h3>
+					<p><strong>Problem:</strong> Needed Wordpress Site</p>
+					<p><strong>Solution:</strong> Used a Themify template to provide a custom design and ease of maintaining.</p>
+					<p><strong>Our Work:</strong> Word Press installation and upkeep. Web hosting.</p>
+					<p><strong>Go There: </strong><a href="https://designbykorigan.com/" target="_blank">DesignByKorigan.com</a></p>
 				</div>
 			</div>
 		</div>
